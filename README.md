99409c707f8498e3864ace38e5d6e48a59f652a2
nfailor <nfailor@gmail.com>
2023-10-10 21:58:10 -0500

    Added comments to index.html and script.js; updated README.md however not complete.

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # daily-planner
+
+## NAME
+The name of this webpage is Daily Planner.
+
+## DESCRIPTION
+This webpage is designed to display a daily 9-5 schedule that allows you to place events in each hour block, and save them to allow for the data to persist on webpage reload. Additionally, each hour block will be dynamically displayed in relation to the current time. If the current time is 11am, then the 9am and 10am hour blocks will be greyed out to display past hour blocks, the 11am hour block will be red to display the current hour, and the 12pm-5pm hour blocks will be green to display future hour blocks. 
+
+## PROGRAMS USED
+I used HTML and CSS to create the header and nav contents of this webpage and javascript (specifically Jquery) to create the daily planner content. Git/Github were used as version control and deployed to Github Pages.
+
+## INSTALLATION
+This is a website only and does not need installation. it is currently hosted in github and you can access it at Github; XX
+
+## USAGE
+This webpage is used to save and review planned events in a daily planner, which is reactive to the current time. See webpage below:
+
+![alt text](assets/images/index.JPG)
+
+To be able to add an event to any hour block, just click into the box and add the content, then click the save button to the right of the corresponding hour block, as seen below:
+
+![alt text](assets/images/event.JPG)
+
+Upon saving the information onto the webpage, you can open the localstorage to see that the information has saved, as seen below:
+
+![alt text](assets/images/localstorage.JPG)
+
+Finally, upon refresh of the page, the user can see the content still displayed where they entered it, as seen below:
+
+![alt text](assets/images/final.JPG)
+
+## SUPPORT AND CONTRIBUTION
+If you have any questions or concerns, please reach out to nfailor@gmail.com
+
+## AUTHORS AND ACKNOWLEDGEMENT
+I am Nick Failor, the author of this project.
+
+## LICENSE
+N/A
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,51 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+    // the below function is activated upon click of the save button, which then saves the user input and corresponding hour block to the local storage
+    $('.saveBtn').on( "click", function(event) {
+        event.preventDefault();
+
+        var userInput = $(this).siblings(".description").val();
+
+        var hourId = $(this).parent().attr('id');
+
+        localStorage.setItem(hourId, userInput);
+        
+    });
+    
+    // the below lines are placed to properly display the current date and time. 
+    var today = dayjs();
+    $('#currentDay').text(today.format('MMM D, YYYY; HH:mm:ss'));
+
+    var currentTime = today.format('HH');
+
+    // the below function is essentially determining the hour blocks' visual display based on the current time based on the 24h clock. If the current time is greater than the hour block, that hour block would be in the past, for example. 
+    $('.time-block').each(function() {
+
+        var timeBlock = $(this).attr('id').split("-")[1];
+
+        if (currentTime > timeBlock) {
+            $(this).addClass("past");
+        } else if (currentTime < timeBlock) {
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("present");
+        }
+      });
+
+    // the below function is what will allow for persisting data to remain on each hour block. for example, if user places "lunch" on the 12 hour block and saves it, that data will appear even after the webpage is reloaded
+    function loadLocalStorageData() {
+        $('.time-block').each(function() {
+            var hourId = $(this).attr('id');
+            var userInput = localStorage.getItem(hourId);
+    
+            if (userInput) {
+                $(this).find(".description").val(userInput);
+            }
+        })};
+    
+    // the below function is what activates the overall javascript
+    $(document).ready(function() {
+        loadLocalStorageData();
+    })
+    
   });
-  
